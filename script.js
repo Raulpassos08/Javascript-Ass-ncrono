7e3bc70b89a8e6c30f18410b4d6ada9b49f46988
Raulpassos08 <raulpassos08@gmail.com>
2024-08-08 14:45:52 -0300

    Estudando then()

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-const paragrafos = document.querySelectorAll("p")
+// Javascript só executya um comando por vez
+// Em poucas palavras, podemos olhar para o código assíncrono como um código que inicia uma tarefa agora e a termina mais tarde. Vamos elaborar sobre isso à medida que avançamos no artigo, mas antes disso, vamos aprender sobre código síncrono — a contraparte do código assíncrono.
+// JavaScript, por natureza, é uma linguagem síncrona. E isso significa que JavaScript pode executar apenas um código por vez — de cima para baixo.
 
-let textoCompleto = ''
+//  Considere o código abaixo:
 
-paragrafos.forEach( p => {
-    textoCompleto += p.textContent
-})
+console.log("logging line 1");
 
-const caracteres = textoCompleto.length
+console.log("logging line 2");
 
-console.log(caracteres)
+console.log("logging line 3");
+
+// Por padrão, o JavaScript executa o código acima de forma síncrona. E isso significa linha por linha. Então a linha 1 não pode ser executada antes da linha 2, e a linha dois não pode ser executada antes da linha 3.
+
+//  new Promise()
+// Promise é uma função construtora de promessas. Existem dois resultados possiveis de uma promessa,
+// ela pode ser resolvida, com a execução do primeiro argumento, ou rejeitada e o segundo argumento for ativado.
+const promessa = new Promise(function (resolve, reject) {
+  let condicao = true;
+  if (condicao) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+console.log(promessa);
+
+// then()
+// O poder das Promisses está no metodo then() do seu protótipo. O Callback deste método só será
+// ativado quando a promise for resolvida. O argumento do callback será o valor passsado na função resolve.
+const promessa = new Promise(function (resolve, reject) {
+  let condicao = true;
+  if (condicao) {
+    resolve("Deu certo, misera");
+  } else {
+    reject("ERRO");
+  }
+});
+
+promessa.then((resolucao) => {
+  console.log(resolucao);
+});
